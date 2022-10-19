4a96c0ec3ba677ebb63d758ed4c8f31bfc304fa4
kh1ld <alsofi.kh@gmail.com>
2022-10-20 02:34:45 +0300

    n

--- install.bat
+++ install.bat
@@ -2,7 +2,7 @@
 if exist Odix_ProPlus (
 	  
 	  git config --global --add safe.directory D:/Odix_ProPlus
-	  cd D:\Odix_ProPlus 
+	  cd D:\Odix_ProPlus
      git pull https://github.com/kh1ld/Odix_ProPlus_Set.git
 ) else (
     git clone https://github.com/kh1ld/Odix_ProPlus_Set.git Odix_ProPlus
